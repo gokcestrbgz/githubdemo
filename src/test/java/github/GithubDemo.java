dfbbd947012e6d4bee5f686869ea77295a4c7bb8
gokcestrbgz <rgokcekadioglu@gmail.com>
2019-07-16 13:13:13 -0500

    new codes added

--- src/test/java/github/GithubDemo.java
+++ src/test/java/github/GithubDemo.java
@@ -3,5 +3,26 @@ package github;
 public class GithubDemo {
     public static void main(String[] args) {
         System.out.println("hello github");
+
+
+        String s = "hahaha";
+
+        for(int i =0; i<s.length(); i++){
+            System.out.println(s.charAt(i));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
